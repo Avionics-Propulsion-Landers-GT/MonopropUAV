024ed1a2b6bbf4a8921fedae0dc96fcbc68a8e79
Kyle Law <klaw41706@gmail.com>
2024-11-08 20:42:09 -0500

    Temp

--- Algorithms/TVC/PathFollowing.py
+++ Algorithms/TVC/PathFollowing.py
@@ -1,1 +1,8 @@
 # Algorithm for coming up with which points on the path to target
+from HermiteSpline import createPoints
+
+start_point = (7, 4, 0)
+initial_velo = (3, 5, 12)
+end_point = (0, 0, 45)
+
+x, y, z = createPoints(start_point, initial_velo, end_point)
