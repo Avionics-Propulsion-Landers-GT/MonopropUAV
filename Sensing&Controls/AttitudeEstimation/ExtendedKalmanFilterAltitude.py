fd0222e746682c662f9a0fbb38e2ca2325dc0fae
kchandana6 <kchandana6@gatech.edu>
2025-02-12 20:59:01 -0500

    new altitude ekf

--- Sensing&Controls/AttitudeEstimation/ExtendedKalmanFilterAltitude.py
+++ Sensing&Controls/AttitudeEstimation/ExtendedKalmanFilterAltitude.py
@@ -65,89 +65,14 @@ class ExtendedKalmanFilterAltitude():
         return
 
     def state_transition_function(self):
-        euler_angular_velocity = self.quaternions_to_euler_angular_velocities(self.euler_to_quaternion(self.previous_state), self.euler_to_quaternion(self.state), self.delta_time)
-        omega_matrix = np.array([[0, -euler_angular_velocity[0], -euler_angular_velocity[1], -euler_angular_velocity[2]], 
-                                 [euler_angular_velocity[0], 0, euler_angular_velocity[2], -euler_angular_velocity[1]], 
-                                 [euler_angular_velocity[1], -euler_angular_velocity[2], 0, euler_angular_velocity[0]], 
-                                 [euler_angular_velocity[2], euler_angular_velocity[1], -euler_angular_velocity[0], 0]])
-
-        next_state = self.quaternion_normalize(self.euler_to_quaternion(self.state) + 0.5 * self.delta_time * omega_matrix @ self.euler_to_quaternion(self.state))
-        return self.quaternion_to_euler(next_state)
+        velocityz = (self.state - self.previous_state) / self.delta_time
+        return velocityz
     
     def state_transition_jacobian(self):
         return np.eye(np.size(self.state))
     
     def measurement_prediction_function(self):
-        euler_angular_velocity = self.get_extrinsic_rotation_matrix(self.state) @ self.quaternions_to_euler_angular_velocities(self.euler_to_quaternion(self.previous_state), self.euler_to_quaternion(self.state), self.delta_time)
-        gravity = self.euler_normalize(self.get_extrinsic_rotation_matrix(self.state) @ np.array([0, 0, -9.81]))
-        mag = self.euler_normalize(self.get_extrinsic_rotation_matrix(self.state) @ np.array([0.00005, 0, 0]))
-
-        return np.array([euler_angular_velocity[0], euler_angular_velocity[1], euler_angular_velocity[2],
-                         gravity[0], gravity[1], gravity[2],
-                         mag[0], mag[1], mag[2]])
-
+        return self.state
     def measurement_prediction_jacobian(self):
-        jacobian = np.zeros((self.measurement_size, np.size(self.state)))
-        jacobian[0][0] = 1
-        jacobian[1][1] = 1
-        jacobian[2][2] = 1
+        jacobian = np.ones((self.measurement_size, np.size(self.state)))
         return jacobian
-    
-    def quaternions_to_euler_angular_velocities(self, q1, q2, dt):
-        return 2 * self.quaternion_multiply(q2, self.quaternion_inverse(q1))[1:4] / dt
-
-    # euler in the form (x,y,z)
-    def euler_to_quaternion(self, euler):
-        return np.array([math.cos(euler[0] / 2) * math.cos(euler[1] / 2) * math.cos(euler[2] / 2) + math.sin(euler[0] / 2) * math.sin(euler[1] / 2) * math.sin(euler[2] / 2),
-                         math.sin(euler[0] / 2) * math.cos(euler[1] / 2) * math.cos(euler[2] / 2) - math.cos(euler[0] / 2) * math.sin(euler[1] / 2) * math.sin(euler[2] / 2),
-                         math.cos(euler[0] / 2) * math.sin(euler[1] / 2) * math.cos(euler[2] / 2) + math.sin(euler[0] / 2) * math.cos(euler[1] / 2) * math.sin(euler[2] / 2),
-                         math.cos(euler[0] / 2) * math.cos(euler[1] / 2) * math.sin(euler[2] / 2) - math.sin(euler[0] / 2) * math.sin(euler[1] / 2) * math.cos(euler[2] / 2)])
-    
-    # quaternion in the form (w,q1,q2,q3) where w is the real rotation component and q1,q2,q3 are the vector components
-    def quaternion_to_euler(self, quaternion):
-        return np.array([math.atan2(2 * (quaternion[0] * quaternion[1] + quaternion[2] * quaternion[3]), (1 - 2 * (quaternion[1] * quaternion[1] + quaternion[2] * quaternion[2]))),
-                         math.asin(2 * (quaternion[0] * quaternion[2] - quaternion[3] * quaternion[1])),
-                         math.atan2(2 * (quaternion[0] * quaternion[3] + quaternion[1] * quaternion[2]), (1 - 2 * (quaternion[2] * quaternion[2] + quaternion[3] * quaternion[3])))])
-    
-    def quaternion_normalize(self, quaternion):
-        norm = math.sqrt(quaternion[0] * quaternion[0] + quaternion[1] * quaternion[1] + quaternion[2] * quaternion[2] + quaternion[3] * quaternion[3])
-        if (norm == 0):
-            return np.array([0, 0, 0, 0])
-        return quaternion / norm
-    
-    def euler_normalize(self, euler):
-        norm = math.sqrt(euler[0] * euler[0] + euler[1] * euler[1] + euler[2] * euler[2])
-        if (norm == 0):
-             return np.array([0, 0, 0])
-        return euler / norm
-    
-    def quaternion_inverse(self, quaternion):
-        norm = math.sqrt(quaternion[0] * quaternion[0] + quaternion[1] * quaternion[1] + quaternion[2] * quaternion[2] + quaternion[3] * quaternion[3])
-        if (norm > 0):
-            return np.array([quaternion[0], -quaternion[1], -quaternion[2], -quaternion[3]]) / norm
-        return np.array([1, 0, 0, 0])
-    
-    def quaternion_multiply(self, q1, q2):
-        return np.array([q1[0] * q2[0] - q1[1] * q2[1] - q1[2] * q2[2] - q1[3] * q2[3],
-                         q1[0] * q2[1] + q1[1] * q2[0] + q1[2] * q2[3] - q1[3] * q2[2],
-                         q1[0] * q2[2] - q1[1] * q2[3] + q1[2] * q2[0] + q1[3] * q2[1],
-                         q1[0] * q2[3] + q1[1] * q2[2] - q1[2] * q2[1] + q1[3] * q2[0]])
-    
-    #euler in (x,y,z)
-    def get_extrinsic_rotation_matrix(self, euler):
-        return self.get_extrinsic_z_rotation(euler[2]) @ self.get_extrinsic_y_rotation(euler[1]) @ self.get_extrinsic_x_rotation(euler[0])
-    
-    def get_extrinsic_x_rotation(self, x):
-        return np.array([[1, 0, 0],
-                         [0, math.cos(x), -math.sin(x)],
-                         [0, math.sin(x), math.cos(x)]])
-    
-    def get_extrinsic_y_rotation(self, y):
-        return np.array([[math.cos(y), 0, math.sin(y)],
-                         [0, 1, 0],
-                         [-math.sin(y), 0, math.cos(y)]])
-    
-    def get_extrinsic_z_rotation(self, z):
-        return np.array([[math.cos(z), -math.sin(z), 0],
-                         [math.sin(z), math.cos(z), 0],
-                         [0, 0, 1]])
