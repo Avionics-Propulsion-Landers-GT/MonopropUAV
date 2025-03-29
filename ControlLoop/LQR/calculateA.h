13f1b2988ac37300299cc2665fe847f429f75258
sboebel2024 <sboebel3@gatech.edu>
2025-03-29 18:06:05 -0400

    calculateA + calculateB files added

---
+++ ControlLoop/LQR/calculateA.h
@@ -0,0 +1,23 @@
+#ifndef CALCULATE_A_H
+#define CALCULATE_A_H
+
+#include "../Matrix.h"
+#include "../Vector.h"
+
+// Computes the A matrix (12x12) from linearization
+Matrix calculateA(
+    double m,
+    double f,
+    double cDrag,
+    double areaVar,
+    const Vector& full_state,   // size 12
+    const Vector& full_input,   // size 7
+    const Vector& rc,           // size 3
+    const Vector& rt,           // size 3
+    const Matrix& inertia,      // size 3x3
+    const Matrix& inertia_s,
+    const Matrix& inertia_a,
+    const Matrix& inertia_b
+);
+
+#endif // CALCULATE_A_H
---
+++ ControlLoop/LQR/calculateB.h
@@ -0,0 +1,23 @@
+#ifndef CALCULATE_B_H
+#define CALCULATE_B_H
+
+#include "../Matrix.h"
+#include "../Vector.h"
+
+// Computes the B matrix (12x7) from linearization
+Matrix calculateB(
+    double m,
+    double f,
+    double cDrag,
+    double areaVar,
+    const Vector& full_state,   // size 12
+    const Vector& full_input,   // size 7
+    const Vector& rc,           // size 3
+    const Vector& rt,           // size 3
+    const Matrix& inertia,      // size 3x3
+    const Matrix& inertia_s,
+    const Matrix& inertia_a,
+    const Matrix& inertia_b
+);
+
+#endif // CALCULATE_B_H
