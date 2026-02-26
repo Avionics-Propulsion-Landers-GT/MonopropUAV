b4aaffa980cbef94c13bff984a1d44482dfd139d
FarkasJoseph <16584585+FarkasJoseph@users.noreply.github.com>
2026-02-26 11:16:49 -0500

    ultra_short direct_descent

--- Algorithms/LosslessConvexification/rust_lossless/src/main.rs
+++ Algorithms/LosslessConvexification/rust_lossless/src/main.rs
@@ -27,13 +27,13 @@ struct SimpleMetricsContext {
 }
 
 fn main() {
-    //  let initial_position = [0.0, 0.0, 50.0];
-    let initial_position = [10.0, 20.0, 50.0];
+     let initial_position = [0.0, 0.0, 50.0];
+    // let initial_position = [10.0, 20.0, 50.0];
 
-    // let max_velocity = 500.0;
-    let max_velocity = 5.0;
+    let max_velocity = 500.0;
+    // let max_velocity = 5.0;
 
-    let min_time_s: f64 = 11.95;
+    let min_time_s: f64 = 7.00;
 
     let mut zoh_solver = LosslessSolver {
         landing_point: [0.0, 0.0, 0.0],
@@ -50,7 +50,7 @@ fn main() {
         coarse_line_search_delta_t: 0.1,
         fine_line_search_delta_t: 0.01,
         coarse_delta_t: 0.05,
-        fine_delta_t: 0.4,
+        fine_delta_t: 0.2,
         use_glide_slope: true,
         glide_slope: 5_f64.to_radians(),
         N: 20,
@@ -72,14 +72,14 @@ fn main() {
         coarse_line_search_delta_t: 0.1,
         fine_line_search_delta_t: 0.01,
         coarse_nodes: 15,
-        fine_nodes: 63,
+        fine_nodes: 10,
         use_glide_slope: true,
         glide_slope: 5_f64.to_radians(),
         ..Default::default()
     };
 
-    let group_name = "offset_limited_descent";
-    let truth_name = "trajectory_zoh_truth_5_vel_limit_offset.csv";
+    let group_name = "direct_descent";
+    let truth_name = "trajectory_zoh_truth.csv";
     let run_type = "ultra_short";
     // let solver_groups_to_run: Vec<&str> = vec!["zoh", "cgl"];
     // let solver_groups_to_run: Vec<&str> = vec!["cgl"];
