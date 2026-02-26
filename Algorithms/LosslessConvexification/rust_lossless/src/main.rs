96718c4da8691e67add51e4e8338b37bf1ee9426
FarkasJoseph <16584585+FarkasJoseph@users.noreply.github.com>
2026-02-26 12:22:52 -0500

    offset short

--- Algorithms/LosslessConvexification/rust_lossless/src/main.rs
+++ Algorithms/LosslessConvexification/rust_lossless/src/main.rs
@@ -50,7 +50,7 @@ fn main() {
         coarse_line_search_delta_t: 0.1,
         fine_line_search_delta_t: 0.01,
         coarse_delta_t: 0.05,
-        fine_delta_t: 0.0125,
+        fine_delta_t: 0.03,
         use_glide_slope: true,
         glide_slope: 5_f64.to_radians(),
         N: 20,
@@ -72,7 +72,7 @@ fn main() {
         coarse_line_search_delta_t: 0.1,
         fine_line_search_delta_t: 0.01,
         coarse_nodes: 15,
-        fine_nodes: 46,
+        fine_nodes: 31,
         use_glide_slope: true,
         glide_slope: 5_f64.to_radians(),
         ..Default::default()
@@ -80,10 +80,10 @@ fn main() {
 
     let group_name = "offset_descent";
     let truth_name = "trajectory_zoh_truth.csv";
-    let run_type = "long";
-    let solver_groups_to_run: Vec<&str> = vec!["zoh", "cgl"];
+    let run_type = "short";
+    // let solver_groups_to_run: Vec<&str> = vec!["zoh", "cgl"];
     // let solver_groups_to_run: Vec<&str> = vec!["cgl"];
-    // let solver_groups_to_run: Vec<&str> = vec!["zoh"];
+    let solver_groups_to_run: Vec<&str> = vec!["zoh"];
     let fine_timing_samples_per_group = 10;
     let comparison_nodes = 100;
 
