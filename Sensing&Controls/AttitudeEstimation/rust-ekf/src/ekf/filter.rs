2160be09a819f38d46a4f435ac28a74a75f8e5bd
aneesh-iyer29 <aneesh.iyer29@gmail.com>
2026-03-10 18:53:16 -0400

    anyis testing data

--- Sensing&Controls/AttitudeEstimation/rust-ekf/src/ekf/filter.rs
+++ Sensing&Controls/AttitudeEstimation/rust-ekf/src/ekf/filter.rs
@@ -47,62 +47,40 @@ impl<T: EKFModel> ExtendedKalmanFilter<T> {
             self.model
                 .state_transition_function(&self.state, self.delta_time);
 
-        let f_jac = self
+        let transition_jacobian = self
             .model
             .state_transition_jacobian(&self.previous_state, self.delta_time);
 
-        self.error_covariance = f_jac.dot(&self.error_covariance).dot(&f_jac.t())
+        self.error_covariance = transition_jacobian.dot(&self.error_covariance).dot(&transition_jacobian.t())
             + &self.process_noise_covariance;
     }
 
-    fn sync_delta_time_from_model(&mut self) {
-        if let Some(model_dt) = self.model.delta_time() {
-            if model_dt.is_finite() && model_dt > 0.0 {
-                self.delta_time = model_dt;
-            }
-        }
-    }
+    pub fn update(&mut self, data: &[f64]) {
+        let measurement = self.model.parse_data(data);
 
-    fn apply_measurement_update(&mut self, measurement: Array1<f64>) {
-        let h_pred = self.model.measurement_prediction_function(&self.state);
-        let residual = &measurement - &h_pred;
+        let prediction = self.model.measurement_prediction_function(&self.state);
+        let residual = &measurement - &prediction;
 
-        let h_jac = self.model.measurement_prediction_jacobian(&self.state);
+        let prediction_jacobian = self.model.measurement_prediction_jacobian(&self.state);
 
-        let s = h_jac.dot(&self.error_covariance).dot(&h_jac.t())
+        let s = prediction_jacobian.dot(&self.error_covariance).dot(&prediction_jacobian.t())
             + &self.measurement_noise_covariance;
 
-        // TODO: Make sure this error doesn't impact the filter's ability to run (Can we give some default value on error instead of returning directly)
         let s_inv = match s.inv() {
             Ok(m) => m,
             Err(_) => {
-                self.error_covariance = &self.error_covariance + &(Array2::<f64>::eye(self.state.len()) * 1e-6); // Prevent panic on singular matrix by adding small diagonal before falling back to prediction
+                self.error_covariance += Array2::eye(self.state.len()) * 1e-6; // Prevent panic on singular matrix by adding small diagonal before falling back to prediction
                 return;
-            }
+            } 
         };
 
-        let k = self.error_covariance.dot(&h_jac.t()).dot(&s_inv);
+        let k = self.error_covariance.dot(&prediction_jacobian.t()).dot(&s_inv);
 
         self.previous_state = self.state.clone();
         self.state = &self.state + &k.dot(&residual);
 
         let identity = Array2::eye(self.state.len());
-        self.error_covariance = (identity - k.dot(&h_jac)).dot(&self.error_covariance);
-    }
-
-    pub fn update(&mut self, data: &[f64]) {
-        let measurement = self.model.parse_data(data);
-        self.sync_delta_time_from_model();
-        self.apply_measurement_update(measurement);
-    }
-
-    /// Parse a timestamped measurement, update dt from the model, then run a
-    /// full predict/update cycle using that same sample.
-    pub fn step(&mut self, data: &[f64]) {
-        let measurement = self.model.parse_data(data);
-        self.sync_delta_time_from_model();
-        self.predict();
-        self.apply_measurement_update(measurement);
+        self.error_covariance = (identity - k.dot(&prediction_jacobian)).dot(&self.error_covariance);
     }
 
     pub fn get_state(&self) -> &Array1<f64> {
